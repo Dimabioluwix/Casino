911b527f674c4f540966a083ac25ce10a1cf6aea
Dmitry Arefyev <arefev.dimjan@gmail.com>
2026-05-12 01:04:31 +0300

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vue/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Casino/', // Добавьте эту строку
 })
